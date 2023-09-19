d95bc6020b1d26cd279e5af466fe67c1d797b9cf
dolja <hpuser230820@oulook.com>
2023-09-19 21:55:53 +0300

    Added Task 03

---
+++ Task03/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая будет 
+//выдавать название дня недели по заданному номеру
+// 3-> Среда
+// 5-> Пятница
+
+
+Console.WriteLine("Введите число от одного до семи включительно");
+int weekday = Convert.ToInt32(Console.ReadLine());
+if (weekday == 1)
+    Console.WriteLine($"{weekday} -> Понедельник");
+else if (weekday == 2)
+    Console.WriteLine($"{weekday} -> Вторник");
+else if (weekday == 3)
+    Console.WriteLine($"{weekday} -> Среда");
+else if (weekday == 4)
+    Console.WriteLine($"{weekday} -> Черверг");
+else if (weekday == 5)
+    Console.WriteLine($"{weekday} -> Пятница");
+else if (weekday == 6)
+    Console.WriteLine($"{weekday} -> Суббота");
+else if (weekday == 7)
+    Console.WriteLine($"{weekday} -> Воскресенье");
+else
+   Console.WriteLine("Число не отвечает условиям ввода");
