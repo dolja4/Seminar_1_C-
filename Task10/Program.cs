4d77873bc4f7ac236164dff81eb75f15ca781e1d
dolja <hpuser230820@oulook.com>
2023-09-24 11:25:36 +0300

    Second change Task 10

--- Task10/Program.cs
+++ Task10/Program.cs
@@ -4,30 +4,12 @@
 // 782 -> 8
 // 918 -> 1
 
+
 Console.WriteLine("Введите трехзначное число");
 int number = Convert.ToInt32(Console.ReadLine());
-int secondDigit = SecondDigit(number);
-if (number < 0) number*= -1;
-if (number >= 100 && number <= 999)  
-
-Console.WriteLine($"{number} -> {secondDigit}");
-
-int SecondDigit(int num)
+if (number >= 100 && number <= 999)
 {
-    int interDigit = num / 10;
-    int secondDigit = interDigit %10;
-    return secondDigit;
+int mid = (number % 100 - number % 10) / 10;
+Console.WriteLine($"{number} -> {mid}");
 }
-
-
-// if (number < 0) number*= -1;
-// if (number >= 100 && number <= 999)  
-
-
-// {
-//     int interDigit = number / 10;
-//     int secondDigit = interDigit %10;
-//     Console.WriteLine($" Вторая цифра введенного числа: {secondDigit}");
-// }
-//else Console.WriteLine("Некоректный ввод");
-
+else System.Console.WriteLine("Это число не трехзначное");
