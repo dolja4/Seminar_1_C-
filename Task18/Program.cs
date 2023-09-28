dd14c446c1e7e6313f8081d4f3d42a0df7c875cd
dolja <hpuser230820@oulook.com>
2023-09-28 16:13:25 +0300

    Added Task 18

---
+++ Task18/Program.cs
@@ -0,0 +1,20 @@
+﻿// 18. Напишите программу, кторая по заданному номеру четверти,
+// показывает диапозон возможных координат точек 
+//в этой четверти (x,y)
+
+string RangeCoordinats(int num)
+{
+    if (num == 1) return "x > 0, y > 0";
+    if (num == 2) return "x < 0, y > 0";
+    if (num == 3) return "x > 0, y < 0";
+    if (num == 4) return "x > 0, y < 0";
+    return null;
+}
+Console.WriteLine("Введите номер четвери");
+int quater = Convert.ToInt32(Console.ReadLine());
+
+string getRangeCoordinats = RangeCoordinats(quater);
+
+string result = getRangeCoordinats != null ? $"Заданному номеру четверти соответсвует диапазон {getRangeCoordinats}"
+                                   : $"некоректный ввод";
+Console.WriteLine(result);
