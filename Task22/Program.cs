12c96487eddb9c195b02ded96164534aafb96c7a
dolja <hpuser230820@oulook.com>
2023-09-28 20:02:37 +0300

    Added Task 22

---
+++ Task22/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая принимает на вход число(N)
+// и выдвет таблицу квадратов чисел от 1 до N.
+//5 -> 1,4,9,16, 25
+
+
+void TableSquare(int num)
+{
+    int count = 1;
+    while (count <= num)
+    {
+        Console.WriteLine($"{count,3}    {count * count,7}");
+        count ++;
+    }
+}
+
+Console.WriteLine("Введите целое положительное число");
+int num = Convert.ToInt32(Console.ReadLine());
+
+TableSquare(num);
