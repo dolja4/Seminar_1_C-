0f6984c65cf7b741001d7f8c44ad6827ca0ad58e
dolja <hpuser230820@oulook.com>
2023-10-02 20:29:05 +0300

    Added Task 25

---
+++ Task25/Program.cs
@@ -0,0 +1,22 @@
+﻿/*Задача 25: Напишите цикл, который принимает на вход 
+два числа (A и B) и возводит число A в 
+натуральную степень B.
+3, 5 -> 243 (3⁵)
+2, 4 -> 16*/
+
+Console.WriteLine("Введите первое целое число");
+Console.Write("A: ");
+int numA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите втрое целое число");
+Console.Write("B: ");
+int numB = Convert.ToInt32(Console.ReadLine());
+if (numB < 0) numB*= -1; 
+int count = 1;
+int step = numA;
+while (count < numB)
+    {
+    step = step * numA ;
+    count++;
+    }
+
+Console.WriteLine($" {numA} в степени {numB} -> {step}");c
