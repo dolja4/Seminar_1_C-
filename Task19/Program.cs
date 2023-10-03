6de0efe9d06ad46d65f2762d06eee102c7707775
dolja <hpuser230820@oulook.com>
2023-10-03 12:05:41 +0300

    added change task 19

--- Task19/Program.cs
+++ Task19/Program.cs
@@ -50,3 +50,28 @@ System.Console.WriteLine($"{number} ->  Некорректный ввод");
 //   CheckingNumber(number);
 // }
 // else Console.WriteLine($"Некорректный вввод");
+
+
+// Задаяа 19(3). Палиндром. 5-ти значное число.
+// int num,r,sum=0,t;
+// Console.WriteLine("Введите пятизначное число: ");
+// num = Convert.ToInt32(Console.ReadLine());
+// if(num>9999 && num<100000)
+// {
+//     for(t=num;num!=0;num=num/10)
+//     {
+//         r=num % 10;
+//         sum=sum*10+r;
+//     }
+// if(t==sum)
+// Console.Write($"{t} является палиндромом");
+// else
+// Console.Write($"{t} не является палиндромом");
+// } 
+// else
+// Console.Write("Некорректный ввод");
+
+
+
+
+ 
