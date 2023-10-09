40d11097027f85dfe9b1d710c32760d8682f9027
dolja <hpuser230820@oulook.com>
2023-10-09 17:50:53 +0300

    Added task 42

---
+++ Task42/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 42: Напишите программу, которая будет преобразовывать
+// десятичное число в двоичное.
+// 46 -> 101110
+// 13 -> 1101
+// 2 -> 10
+
+int DecToBaseNum(int number, int baseNum)
+{
+    int result = 0; 
+    int mult = 1;
+    while (number >0)
+    {
+        result += number % baseNum * mult;
+        number /= baseNum;
+        mult *= 10;
+    }
+    return result;
+}
+
+Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите основане системы счисления");
+int baseNumber = Convert.ToInt32(Console.ReadLine());
+
+int res = DecToBaseNum(number, baseNumber);
+
+Console.WriteLine($" - > {res}");
+
+
+
+
