1e879f9cff24935f3121bdc8a7e945d570ba39eb
dolja <hpuser230820@oulook.com>
2023-10-10 11:10:59 +0300

    Added task 41

---
+++ Task41/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 41. Пользователь вводит с клавиатуры М чисел.
+//Посчтитайте, сколько чисел больше 0 ввёл пользователь.
+
+
+Console.WriteLine("Введите число - размер массива");
+int arrsize = Convert.ToInt32(Console.ReadLine());
+
+int[] array = new int[arrsize];
+
+void Massif(int arrsize)
+{
+    for (int i = 0; i < arrsize; i++)
+    {
+        Console.WriteLine($"Введите {i + 1} элемент массива");
+        array[i] = Convert.ToInt32(Console.ReadLine());
+    }
+}
+
+int CountPositiveNum(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0) count++;
+    }
+    return count;
+}
+
+Massif(arrsize);
+Console.WriteLine($"Чисел больше нуля: {CountPositiveNum(array)}");
