3576e173018ee19d45b5a325eb737954d293183d
dolja <hpuser230820@oulook.com>
2023-10-14 12:56:01 +0300

    Task50

--- Task50/Program.cs
+++ Task50/Program.cs
@@ -1,52 +1,80 @@
-﻿// Напишите программу, которая на вход принимает позиции
-// элемента в двухмерном массиве, и возвращает значение этого 
-//элемента или же указание, что такого элемента нет.
+﻿// Задача 50. Напишите программу, которая на вход принимает
+//позиции элемента в дыухмерном массиве, и 
+//возвращает значение этого элемента или же указание,
+//что такого элемента нет.
 
-
-
-
-int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
-{
-    //                        0      1
-    int[,] matrix = new int[rows, columns];
-    Random rnd = new Random();
-
-    for (int i = 0; i < matrix.GetLength(0); i++)
+void PrintArray (int [,] matrix)
     {
+      for (int i = 0; i < matrix.GetLength(0); i++)
+    {       
         for (int j = 0; j < matrix.GetLength(1); j++)
         {
-            matrix[i, j] = rnd.Next(min, max + 1);
+           Console.Write($"{matrix[i, j]}\t");
         }
-    }
-
-    return matrix;
-}
-
-void PrintMatrix(int[,] matrix)
-{
-    for (int i = 0; i < matrix.GetLength(0); i++)
-    {
-        //Console.Write("|");
-        for (int j = 0; j < matrix.GetLength(1); j++)
-        {
-            Console.Write($"{matrix[i, j],5}");
-        }
-        //Console.WriteLine("  |");
         Console.WriteLine();
     }
-}
+    }    
+int[,] CreateIncreasingMatrix(int n, int m, int k)
+    {
+         int[,] matrix = new int[n, m];
+         int num = 1;
+         for (int i = 0; i < n; i++)
+         {
+           for (int j = 0; j < m; j++)
+           {
+           matrix[i, j] = num;
+           num += k;                   
+           }      
+          }
+      return matrix;
+     }
+int[] FindNumberByPosition (int [,] matrix, int rowPosition, int columnPosition)
+    {  
+    int[] mtrix = new int[2];
+    if (rowPosition > matrix.GetLength(0) || columnPosition > matrix.GetLength(1))
+    {
+        return mtrix;
+    }
+    else
+    {
+        mtrix[0] = matrix[rowPosition - 1, columnPosition - 1];
+    }
+    return mtrix;
+    }
 
-Console.WriteLine("Введите индекс строки");
-int n = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите индекс столбца");
-int m = Convert.ToInt32(Console.ReadLine());
+void PrintCheckIfError (int[] results, int X, int Y)     
+     {      
+    int x = X;
+    int y = Y; 
+    int res =  results[0];      
+     if (res != 0) 
+     Console.WriteLine($"The number in [{x}, {y}] is {res}");    
+    else
+    Console.WriteLine("There is no such index");              
+    }   
 
-int[,] array2d = CreateMatrixRndInt(5, 5, -100, 100);
-PrintMatrix(array2d);
-if(n > array2d.GetLength(0)|| m > array2d.GetLength(1))
-{
-    Console.WriteLine($"{n},{m} -> такого элемента в массиве нет");
-}
-else if(n < 0 || m < 0)Console.WriteLine("Индекс меньше нуля");
+//    void Main(string[] args) 
+//    {
+        int n, m, k, x, y;
+        if (args.Length >= 5) 
+        {
+           n = int.Parse(args[0]);
+           m = int.Parse(args[1]);
+           k = int.Parse(args[2]);
+           x = int.Parse(args[3]);
+           y = int.Parse(args[4]);
+        } 
+        else 
+        {           
+           n = 3;
+           m = 4;
+           k = 2;
+           x = 3;
+           y = 3;
+        }
 
-else Console.WriteLine($"{n},{m} -> {array2d[n,m]}");
+        int[,] result = CreateIncreasingMatrix(n, m, k);
+        PrintArray(result);
+        PrintCheckIfError(FindNumberByPosition(result, x, y), x, y);
+//    }
+ 
