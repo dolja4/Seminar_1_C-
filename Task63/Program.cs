7aba4c6c841ccdec4c071126dd0ef23605969fb5
dolja <hpuser230820@oulook.com>
2023-10-19 14:59:32 +0300

    Added change Task 63

--- Task63/Program.cs
+++ Task63/Program.cs
@@ -20,20 +20,3 @@ void PrintNaturalNumbers(int num)
     PrintNaturalNumbers(num - 1);
     Console.Write($"{num} ");
 }
-Console.WriteLine("Введите натуральное число: ");
-int number = Convert.ToInt32(Console.ReadLine());
-
-if(number < 1)
-{
-    Console.WriteLine("Некорректный ввод!");
-    return;
-}
-
-PrintNaturalNumbers(number);
-
-void PrintNaturalNumbers(int num)
-{
-    if(num == 0) return;
-    PrintNaturalNumbers(num - 1);
-    Console.Write($"{num} ");
-}
